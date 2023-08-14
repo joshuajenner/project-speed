ff03c0aa0b3f9ef8aaff9abc27ff351b4b0cdb4b
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-14 11:19:24 +0800

    load values from resource

--- noise_generator/noise_generator.gd
+++ noise_generator/noise_generator.gd
@@ -1,11 +1,34 @@
 extends Control
 
+@onready var file_input: LineEdit = %FileInput
+@onready var message_output: LineEdit = %MessageOutput
+
+@onready var noise_type_input: OptionButton = %NoiseTypeInput
+@onready var seed_input: SpinBox = %SeedInput
+@onready var frequency_input: SpinBox = %FrequencyInput
+@onready var offset_x_input: SpinBox = %OffsetXInput
+@onready var offset_y_input: SpinBox = %OffsetYInput
+@onready var offset_z_input: SpinBox = %OffsetZInput
 
 @onready var cellular_distance_function_input: OptionButton = %CDFInput
+@onready var cellular_jitter_input: SpinBox = %CellularJitterInput
 @onready var cellular_return_type_input: OptionButton = %CRTInput
-@onready var domain_warp_fractal_type_input = %DomainWarpFractalTypeInput
-@onready var noise_type_input: OptionButton = %NoiseTypeInput
+
+@onready var domain_warp_amplitude_input: SpinBox = %DomainWarpAmplitudeInput
+@onready var domain_warp_enabled_input: CheckBox = %DomainWarpEnabledInput
+@onready var domain_warp_fractal_gain_input: SpinBox = %DomainWarpFractalGainInput
+@onready var domain_warp_fractal_lacunarity_input: SpinBox = %DomainWarpFractalLacunarityInput
+@onready var domain_warp_fractal_octaves_input: SpinBox = %DomainWarpFractalOctavesInput
+@onready var domain_warp_fractal_type_input: OptionButton = %DomainWarpFractalTypeInput
+@onready var domain_warp_frequency_input: SpinBox = %DomainWarpFrequencyInput
+@onready var domain_warp_type_input: OptionButton = %DomainWarpTypeInput
+
+@onready var fractal_gain_input: SpinBox = %FractalGainInput
+@onready var fractal_lacunarity_input: SpinBox = %FractalLacunarityInput
+@onready var fractal_octaves_input: SpinBox = %FractalOctavesInput
+@onready var fractal_ping_pong_strength_input: SpinBox = %FractalPingPongStrengthInput
 @onready var fractal_type_input: OptionButton = %FractalTypeInput
+@onready var fractal_weighted_strength_input: SpinBox = %FractalWeightedStrengthInput
 
 
 const noise_types: Array = [
@@ -62,12 +85,40 @@ func _ready():
 
 func load_values_from_resource() -> void:
 	noise_type_input.select(noise_resource.noise_type)
-
+	seed_input.value = noise_resource.seed
+	frequency_input.value = noise_resource.frequency
+	offset_x_input.value = noise_resource.offset.x
+	offset_y_input.value = noise_resource.offset.y
+	offset_z_input.value = noise_resource.offset.z
+	
+	cellular_distance_function_input.select(noise_resource.cellular_distance_function)
+	cellular_jitter_input.value = noise_resource.cellular_jitter
+	cellular_return_type_input.select(noise_resource.cellular_return_type)
+	
+	domain_warp_amplitude_input.value = noise_resource.domain_warp_amplitude
+	domain_warp_enabled_input.button_pressed = noise_resource.domain_warp_enabled
+	domain_warp_fractal_gain_input.value = noise_resource.domain_warp_fractal_gain
+	domain_warp_fractal_lacunarity_input.value = noise_resource.domain_warp_fractal_lacunarity
+	domain_warp_fractal_octaves_input.value = noise_resource.domain_warp_fractal_octaves
+	domain_warp_fractal_type_input.select(noise_resource.domain_warp_fractal_type)
+	domain_warp_frequency_input.value = noise_resource.domain_warp_frequency
+	domain_warp_type_input.select(noise_resource.domain_warp_type)
+	
+	fractal_gain_input.value = noise_resource.fractal_gain
+	fractal_lacunarity_input.value = noise_resource.fractal_lacunarity
+	fractal_octaves_input.value = noise_resource.fractal_octaves
+	fractal_ping_pong_strength_input.value = noise_resource.fractal_ping_pong_strength
+	fractal_type_input.select(noise_resource.fractal_type)
+	fractal_weighted_strength_input.value = noise_resource.fractal_weighted_strength
 
 func init_ui() -> void:
-	init_noise_options()
+	add_options_to_input(noise_types, noise_type_input)
+	add_options_to_input(cellular_distance_functions, cellular_distance_function_input)
+	add_options_to_input(cellular_return_types, cellular_return_type_input)
+	add_options_to_input(domain_warp_fractal_types, domain_warp_fractal_type_input)
+	add_options_to_input(domain_warp_types, domain_warp_type_input)
+	add_options_to_input(fractal_types, fractal_type_input)
 
-
-func init_noise_options() -> void:
-	for noise in noise_types:
-		noise_type_input.add_item(noise[0], noise[1])
+func add_options_to_input(options: Array, input: OptionButton) -> void:
+	for option in options:
+		input.add_item(option[0], option[1])
