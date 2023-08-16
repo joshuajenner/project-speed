eadeb237ef3a1f3be8287acee88cb847b242dc27
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-16 18:15:32 +0800

    started adding second layer

--- test/generated_gridmap.gd
+++ test/generated_gridmap.gd
@@ -5,19 +5,16 @@ extends Node3D
 @onready var grid_map: GridMap = $GridMap
 
 
-var map_size := Vector2i(64, 64)
-var mapped_cells: Array = []
+var map_size := Vector2i(128, 128)
+var mapped_cells_1: Array = []
+var mapped_cells_2: Array = []
 
-
-var zero = 0
-var ten = 0
-var twenty_two = 0
-var sixteen = 0
 
 func _ready():
-	var myQuaternion = Quaternion(Vector3(0, 1, 0), deg_to_rad(-90))
-	var cell_item_orientation = grid_map.get_orthogonal_index_from_basis(Basis(myQuaternion))
-	print(cell_item_orientation)
+	pass
+#	var myQuaternion = Quaternion(Vector3(0, 1, 0), deg_to_rad(-90))
+#	var cell_item_orientation = grid_map.get_orthogonal_index_from_basis(Basis(myQuaternion))
+#	print(cell_item_orientation)
 
 
 func _on_noise_generator_noise_generated(noise):
@@ -26,18 +23,28 @@ func _on_noise_generator_noise_generated(noise):
 
 
 func generate_tilemap(noise: FastNoiseLite):
-	mapped_cells = []
+	mapped_cells_1 = []
+	mapped_cells_2 = []
 	tile_map.clear()
 	for x in range(-map_size.x, map_size.x):
 		for y in range(-map_size.y, map_size.y):
 			var noise_level = (noise.get_noise_2d(x, y) + 1) / 2
-			if noise_level > 0.50:
-				mapped_cells.append(Vector2i(x, y))
-				mapped_cells.append(Vector2i(x-1, y))
-				mapped_cells.append(Vector2i(x, y-1))
-				mapped_cells.append(Vector2i(x-1, y-1))
+			if noise_level > 0.60:
+				mapped_cells_2.append(Vector2i(x, y))
+				mapped_cells_2.append(Vector2i(x-1, y))
+				mapped_cells_2.append(Vector2i(x, y-1))
+				mapped_cells_2.append(Vector2i(x-1, y-1))
 				
-	tile_map.set_cells_terrain_connect(0, mapped_cells, 0, 0)
+			elif noise_level > 0.40:
+				mapped_cells_1.append(Vector2i(x, y))
+				mapped_cells_1.append(Vector2i(x-1, y))
+				mapped_cells_1.append(Vector2i(x, y-1))
+				mapped_cells_1.append(Vector2i(x-1, y-1))
+				
+				
+				
+	tile_map.set_cells_terrain_connect(0, mapped_cells_1, 0, 0)
+	tile_map.set_cells_terrain_connect(1, mapped_cells_2, 0, 0)
 	
 
 func map_tilemap_to_gridmap() -> void:
@@ -45,11 +52,20 @@ func map_tilemap_to_gridmap() -> void:
 	for x in range(-map_size.x, map_size.x):
 		for y in range(-map_size.y, map_size.y):
 			grid_map.set_cell_item(Vector3i(x, 0, y), 0, 0)
+			# First Layer
 			var cell_atlas_coords: Vector2i = tile_map.get_cell_atlas_coords(0, Vector2i(x,y))
 			if cell_atlas_coords != Vector2i(-1, -1):
 				var item_index = get_item_from_cell_atlas(cell_atlas_coords)
 				var orientation_index = get_orientation_from_cell_atlas(cell_atlas_coords)
 				grid_map.set_cell_item(Vector3i(x, 1, y), item_index, orientation_index)
+			
+			# Second Layer
+			cell_atlas_coords = tile_map.get_cell_atlas_coords(1, Vector2i(x,y))
+			if cell_atlas_coords != Vector2i(-1, -1):
+				var item_index = get_item_from_cell_atlas(cell_atlas_coords)
+				var orientation_index = get_orientation_from_cell_atlas(cell_atlas_coords)
+				grid_map.set_cell_item(Vector3i(x, 1, y), item_index, orientation_index)
+
 
 
 func get_item_from_cell_atlas(coords: Vector2i) -> int:
@@ -81,15 +97,3 @@ func get_orientation_from_cell_atlas(coords: Vector2i) -> int:
 		return 16
 		
 	return 0
-	
-#	match coords.x:
-#		0 or 3 or 7 or 12:
-#			return 0
-#		1 or 6 or 10:
-#			return 10
-#		4 or 5 or 9:
-#			return 22
-#		2 or 8 or 11:
-#			return 10
-#
-#	return 0
