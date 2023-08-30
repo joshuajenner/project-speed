07356d83d406ed90e0aeb8d88303f18d1d77022c
Joshua Jenner <33088839+joshuajenner@users.noreply.github.com>
2023-08-30 12:40:47 +0800

    fixed layer order

--- test/generated_gridmap.gd
+++ test/generated_gridmap.gd
@@ -4,23 +4,35 @@ extends Node3D
 @onready var tile_map: TileMap = %TileMap
 @onready var grid_map: GridMap = $GridMap
 
-var map_size: int = 256
+var chunk_size: int = 512
+var chunk_center: int = chunk_size / 2
+var chunk_current_coord := Vector2i(chunk_center, chunk_center)
+var loaded_chunks: Array[Vector2i] = [chunk_current_coord]
+
 var mapped_cells_0: Array[Array] = []
 var mapped_cells_1: Array[Array] = []
 
 
+
 func _ready():
-	init_cells_arrays()
+	pass
+
+
+func _physics_process(delta):
 	print(Player.current_position)
+
+
+func update_current_chunk_position() -> void:
+	pass
 
 
 func init_cells_arrays() -> void:
 	mapped_cells_0.clear()
 	mapped_cells_1.clear()
-	for x in range(map_size):
+	for x in range(chunk_size):
 		mapped_cells_0.push_back([])
 		mapped_cells_1.push_back([])
-		for y in range(map_size):
+		for y in range(chunk_size):
 			mapped_cells_0[x].push_back(-1)
 			mapped_cells_1[x].push_back(-1)
 
@@ -33,25 +45,25 @@ func _on_noise_generator_noise_generated(noise: FastNoiseLite):
 
 
 func generate_tilemap(noise: FastNoiseLite) -> void:
-	for x in range(0, map_size):
-		for y in range(0, map_size):
+	for x in range(0, chunk_size):
+		for y in range(0, chunk_size):
 			var noise_level = (noise.get_noise_2d(x, y) + 1) / 2
 			if noise_level > 0.60:
-				mapped_cells_0[x][y] = 0
-				mapped_cells_0[max(0, x-1)][y] = 0
-				mapped_cells_0[x][max(0, y-1)] = 0
-				mapped_cells_0[max(0, x-1)][max(0, y-1)] = 0
-			elif noise_level > 0.40:
 				mapped_cells_1[x][y] = 0
 				mapped_cells_1[max(0, x-1)][y] = 0
 				mapped_cells_1[x][max(0, y-1)] = 0
 				mapped_cells_1[max(0, x-1)][max(0, y-1)] = 0
+			elif noise_level > 0.40:
+				mapped_cells_0[x][y] = 0
+				mapped_cells_0[max(0, x-1)][y] = 0
+				mapped_cells_0[x][max(0, y-1)] = 0
+				mapped_cells_0[max(0, x-1)][max(0, y-1)] = 0
 	print("Cells Picked")
 
 
 func solve_tilemap() -> void:
-	for x in range(0, map_size):
-		for y in range(0, map_size):
+	for x in range(0, chunk_size):
+		for y in range(0, chunk_size):
 			if mapped_cells_0[x][y] >= 0:
 				var cell_index: int = get_cell_index_from_surrounding_cells(mapped_cells_0, x, y)
 				mapped_cells_0[x][y] = cell_index
@@ -65,9 +77,9 @@ func get_cell_index_from_surrounding_cells(cell_map: Array, cell_x: int, cell_y:
 	var index: int = 0
 	if cell_map[cell_x][cell_y - 1] >= 0:
 		index += 1
-	if cell_map[min(map_size-1, cell_x + 1)][cell_y] >= 0:
+	if cell_map[min(chunk_size-1, cell_x + 1)][cell_y] >= 0:
 		index += 2
-	if cell_map[cell_x][min(map_size-1, cell_y + 1)] >= 0:
+	if cell_map[cell_x][min(chunk_size-1, cell_y + 1)] >= 0:
 		index += 4
 	if cell_map[cell_x - 1][cell_y] >= 0:
 		index += 8
@@ -81,19 +93,19 @@ func solve_surrounded_cell(cell_map: Array, cell_x: int, cell_y: int) -> int:
 	var index: int = 0
 	if cell_map[cell_x - 1][cell_y - 1] < 0:
 		index += 8
-	if cell_map[min(map_size-1, cell_x + 1)][cell_y - 1] < 0:
+	if cell_map[min(chunk_size-1, cell_x + 1)][cell_y - 1] < 0:
 		index += 1
-	if cell_map[cell_x - 1][min(map_size-1, cell_y + 1)] < 0:
+	if cell_map[cell_x - 1][min(chunk_size-1, cell_y + 1)] < 0:
 		index += 4
-	if cell_map[min(map_size-1, cell_x + 1)][min(map_size-1, cell_y + 1)] < 0:
+	if cell_map[min(chunk_size-1, cell_x + 1)][min(chunk_size-1, cell_y + 1)] < 0:
 		index += 2
 	return index
 
 
 func map_tilemap_to_gridmap() -> void:
 	grid_map.clear()
-	for x in range(0, map_size):
-		for y in range(0, map_size):
+	for x in range(0, chunk_size):
+		for y in range(0, chunk_size):
 			grid_map.set_cell_item(Vector3i(x, 0, y), 0, 0)
 			grid_map.set_cell_item(Vector3i(x, 1, y), mapped_cells_0[x][y], 0)
 			grid_map.set_cell_item(Vector3i(x, 2, y), mapped_cells_1[x][y], 0)
