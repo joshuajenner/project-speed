1b8b2193d447b8be67d610b9bb88ef19a3a58b44
joshuajenner <33088839+joshuajenner@users.noreply.github.com>
2023-09-02 19:49:05 +0800

    more ui stuff

--- autoloads/pause.gd
+++ autoloads/pause.gd
@@ -2,11 +2,24 @@ extends CanvasLayer
 
 var is_paused: bool = false
 
+signal on_paused
+signal on_resume
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		if is_paused:
-			is_paused = false
+			set_resume()
 		else:
-			is_paused = true
-		get_tree().paused = is_paused
+			set_paused()
+
+
+func set_paused() -> void:
+	is_paused = true
+	get_tree().paused = is_paused
+	on_paused.emit()
+
+
+func set_resume() -> void:
+	is_paused = false
+	get_tree().paused = is_paused
+	on_resume.emit()
